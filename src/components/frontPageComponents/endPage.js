8f5317436a026186234417135da9d4f8bc89acc9
oyvindjt <oyvindj@stud.ntnu.no>
2020-02-24 15:17:13 +0100

    added frontpage components

---
+++ src/components/frontPageComponents/endPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./style.css";
+
+
+export class EndPage extends React.Component {
+
+    render() {
+        return ( <div>
+            <div className = "greeting">
+                <p>Takk for ditt bidrag til Lær Kidsa Koding!</p>
+                <p1>Ditt bidrag er nå inne til vurdering. </p1>
+            </div>
+
+            <button className ="closeBtn" > Lukk vindu </button>
+        </div>);
+    };
+};
---
+++ src/components/frontPageComponents/indexPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "style.css";
+
+
+
+export class IndexPage extends React.Component {
+    render() {
+        return ( <div className = "StartPageContainer">
+
+
+            <div className = "quote">
+                <p2>"Programs must be written for people to read, and only incidentally for machines to execute."</p2>
+                <p3>Harold Abelson </p3>
+            </div>
+            <button className = "btn1" > Min side </button>
+                <button className = "btn2"> Rediger en oppgave </button>
+                    <button className = "btn3"> Lag ny oppgave </button>
+
+
+
+
+        </div>);
+
+    };
+    };
