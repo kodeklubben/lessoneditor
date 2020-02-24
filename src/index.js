e16dbeee70816a806fe88bc4745b376e842be604
oyvindjt <oyvindj@stud.ntnu.no>
2020-02-24 14:59:14 +0100

    Added serviceworker

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import "semantic-ui-css/semantic.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
