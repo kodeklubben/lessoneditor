c9143241430e424a19e59514f47616606052864d
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-02-24 11:00:05 +0100

    minor changes

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,13 +3,21 @@ import { Icon } from "semantic-ui-react";
 
 class Button extends React.Component {
   onButtonClick = event => {
+    var test = " ";
+
     this.props.onButtonClickHandler(this.props.output);
+    if (this.test === " ") {
+      this.test = "inverted";
+    } else {
+      this.test = " ";
+    }
   };
 
   render() {
     return (
       <div onClick={this.onButtonClick}>
-        <i className={this.props.icon + " icon"} />
+        {console.log(`${this.props.icon} ${this.props.test} icon`)}
+        <i className={`${this.props.icon} icon`} />
       </div>
     );
   }
