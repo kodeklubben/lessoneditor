2f4ee828c8683c457dc3fe8c9e3f09649db60ce5
Zmrihawy <zmrihawy@stud.ntnu.no>
2020-02-25 09:03:14 +0100

    Set up routing on different pages.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import FormComponent from "./YAMLComponents/FormComponent";
 import Editor from "./mdPreviewComponents/editor";
 import {IndexPage} from "./frontPageComponents/indexPage";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 class App extends React.Component {
 
@@ -11,9 +15,21 @@ class App extends React.Component {
     return (
         /*To render the wanted component, add it to the div below:
         * <FormComponent/>, <Editor/> or <IndexPage/>*/
-      <div className="root container">
-        <IndexPage/>
-      </div>
+      <Router>
+       <Switch>
+          <Route exact path="/">
+            <div className="root container">
+              <IndexPage/>
+            </div>
+          </Route>
+          <Route path="/createNewLesson">
+            <FormComponent/> 
+          </Route>
+          <Route path="/editor">
+           <Editor/>
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
--- src/components/frontPageComponents/indexPage.js
+++ src/components/frontPageComponents/indexPage.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import "./style.css";
-
+import{Link} from "react-router-dom";
 
 
 export class IndexPage extends React.Component {
     render() {
-        return ( <div className = "StartPageContainer">
+        return ( 
+        <div className = "StartPageContainer">
 
 
             <div className = "quote">
                 <p2>"Programs must be written for people to read, and only incidentally for machines to execute."</p2>
                 <p3>Harold Abelson </p3>
             </div>
-            <button className = "btn1" > Min side </button>
-                <button className = "btn2"> Rediger en oppgave </button>
-                    <button className = "btn3"> Lag ny oppgave </button>
-
-
-
-
-        </div>);
+            <button className = "btn1" > <Link to="">Min side</Link> </button>
+                <button className = "btn2"><Link to="">Rediger en oppgave</Link>  </button>
+                    <button className = "btn3"><Link to="/createNewLesson">Lag ny oppgave </Link> </button>
+          
+       </div>
+        
+        );
 
     };
     }
