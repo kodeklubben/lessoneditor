98857e72a474ebb9e5e290fa436d784df52fbd90
BBrekken <brekken.benjamin.aune@gmail.com>
2020-02-25 13:10:07 +0100

    Generate buttons with map

--- src/components/mdPreviewComponents/ControlPanel.js
+++ src/components/mdPreviewComponents/ControlPanel.js
@@ -3,63 +3,35 @@ import Button from "./Button";
 
 const temp = "```";
 
+const buttonConfig = [
+  { icon: "bold", output: "**bold text here** ", title: "" },
+  { icon: "italic", output: " _italic text here_ ", title: "" },
+  { icon: "", output: " {.activity} ", title: "Steg" },
+  { icon: "", output: " {.intro} ", title: "Intro" },
+  { icon: "", output: " `inline code here` ", title: "Inline Code" },
+  { icon: "", output: `${temp}\ncodeblock\n${temp}`, title: "Codeblock" }
+];
+
 class ControlPanel extends React.Component {
   handleButtonClick = value => {
     this.props.handleButtonClick(value);
   };
 
   render() {
+    console.log({ ...buttonConfig[0], icon: "Hei" });
     return (
       <div className="Buttons">
         <div className="ui segment six column grid">
-          <div className="column">
-            <Button
-              icon="bold"
-              output=" **bold text here** "
-              title=""
-              onButtonClick={this.handleButtonClick}
-            />
-          </div>
-          <div className="column">
-            <Button
-              icon="italic"
-              output=" _italic text here_ "
-              title=""
-              onButtonClick={this.handleButtonClick}
-            />
-          </div>
-          <div className="column">
-            <Button
-              icon=""
-              output=" {.activity} "
-              title="Steg"
-              onButtonClick={this.handleButtonClick}
-            />
-          </div>
-          <div className="column">
-            <Button
-              icon=""
-              output=" {.intro} "
-              title="Intro"
-              onButtonClick={this.handleButtonClick}
-            />
-          </div>
-          <div className="column">
-            <Button
-              icon=""
-              output=" `inline code here` "
-              title="Inline Kode"
-              onButtonClick={this.handleButtonClick}
-            />
-          </div>
-          <div className="column">
-            <Button
-              icon=""
-              output={`${temp}\ncodeblock\n${temp}`}
-              title="Code Block Kode"
-              onButtonClick={this.handleButtonClick}
-            />
-          </div>
+          {buttonConfig.map(element => (
+            <div key={element.output} className="column">
+              <Button
+                icon={element.icon}
+                output={element.output}
+                title={element.title}
+                onButtonClick={this.handleButtonClick}
+              />
+            </div>
+          ))}
         </div>
       </div>
     );
