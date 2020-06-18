01818d08a6d3075c3cf3587a16ebc57b044158c9
Michael Johansen <michael.johansen@nav.no>
2020-06-18 16:24:54 +0200

    Ny test på PageButtons

---
+++ frontend/src/components/PageButtons.spec.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PageButtons from "./PageButtons";
+import { shallow } from "enzyme";
+
+it("should call setPageNumber", () => {
+  const setPageNumber = jest.fn();
+  const wrapper = shallow(
+    <PageButtons
+      prevTitle={"Previous"}
+      nextTitle={"Next"}
+      setPageNumber={setPageNumber}
+      err="author"
+      setErr={(e) => {
+        console.log(e);
+      }}
+      state={{}}
+    ></PageButtons>
+  );
+  wrapper.find("button").first().simulate("click");
+  expect(setPageNumber).toHaveBeenCalled();
+});
