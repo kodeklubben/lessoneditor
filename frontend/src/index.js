0da058f065ba986508598ee6709a40fa93dcb904
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-07-02 22:26:43 +0200

    highlights.js working for real (#82)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
-import "highlight.js/styles/default.css";
+import "./highlightsJS.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
