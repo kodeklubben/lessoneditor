b9ed816334a9fa078f9a7ec2d6a610da84727ae6
Michael Johansen <michael.johansen@nav.no>
2020-07-03 19:27:28 +0200

    Fikser litt lengre sessioner (#85)

--- backend/server-gae.js
+++ backend/server-gae.js
@@ -1,8 +1,14 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
+const isAppEngine = require("./utils/isAppEngine");
+const envFile = isAppEngine() ? ".env" : ".env.local";
+require("dotenv").config({
+  path: path.join(__dirname, envFile),
+});
 const init = require("./app");
 const port = process.env.PORT || 3232;
 
+console.log("GITHUB_CLIENT_ID", process.env.GITHUB_CLIENT_ID);
+console.log("GITHUB_CALLBACK_URL", process.env.GITHUB_CALLBACK_URL);
 init().then((app) => {
   app.listen(port, () =>
     console.log(`Server(GAE) is running on http://localhost:${port}`)
