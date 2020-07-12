9b7c0efb3fa3047730e42499df800aa94c9da2b5
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-07-12 14:22:19 +0200

    Update Overview.js

--- frontend/src/components/mypage/Overview.js
+++ frontend/src/components/mypage/Overview.js
@@ -13,14 +13,6 @@ const lessonScreenshots = [
   "lessonsScreenshots/Screenshot 2020-07-12 at 09.43.15.png",
 ];
 
-const lessonScreenshots = [
-  "lessonsScreenshots/Screenshot 2020-07-12 at 09.42.03.png",
-  "lessonsScreenshots/Screenshot 2020-07-12 at 09.42.22.png",
-  "lessonsScreenshots/Screenshot 2020-07-12 at 09.42.41.png",
-  "lessonsScreenshots/Screenshot 2020-07-12 at 09.42.58.png",
-  "lessonsScreenshots/Screenshot 2020-07-12 at 09.43.15.png",
-];
-
 const Overview = () => {
   const context = useContext(UserContext);
   const { lessons } = context.user;
