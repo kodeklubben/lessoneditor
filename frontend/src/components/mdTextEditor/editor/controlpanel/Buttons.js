f816a823740bc13df6701cfa71e4530d56d5c9c7
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-07-17 09:40:48 +0200

    heading works

--- frontend/src/components/mdTextEditor/editor/controlpanel/Buttons.js
+++ frontend/src/components/mdTextEditor/editor/controlpanel/Buttons.js
@@ -63,7 +63,7 @@ const Buttons = ({
     }
   };
 
-  const bold = (
+  const emphasisButton = (
     isOn,
     mdText,
     cursorPositionStart,
@@ -93,6 +93,49 @@ const Buttons = ({
     }
   };
 
+  const heading = (isOn, mdText, cursorPositionStart, output) => {
+    if (
+      output === "## " &&
+      mdText.slice(cursorPositionStart - 3, cursorPositionStart) === output &&
+      !isOn
+    ) {
+      isOn = !isOn;
+      mdText =
+        mdText.slice(0, cursorPositionStart - 3) +
+        "# " +
+        mdText.slice(cursorPositionStart);
+      setMdText(mdText);
+      cursorPositionStart -= 1;
+      setCursorPosition(cursorPositionStart, cursorPositionStart);
+      return { isOn, mdText, cursorPositionStart };
+    } else if (output === "## " && !isOn) {
+      mdText =
+        mdText.slice(0, cursorPositionStart) +
+        output +
+        mdText.slice(cursorPositionStart);
+      setMdText(mdText);
+      cursorPositionStart += output.length;
+      setCursorPosition(cursorPositionStart, cursorPositionStart);
+      return { isOn, mdText, cursorPositionStart };
+    } else if (output === "## " && isOn) {
+      if (mdText.slice(cursorPositionStart - 2, cursorPositionStart) === "# ") {
+        mdText =
+          mdText.slice(0, cursorPositionStart - 2) +
+          mdText.slice(cursorPositionStart);
+        setMdText(mdText);
+        cursorPositionStart -= 2;
+        setCursorPosition(cursorPositionStart, cursorPositionStart);
+        isOn = !isOn;
+        return { isOn, mdText, cursorPositionStart };
+      } else {
+        isOn = !isOn;
+        return { isOn, mdText, cursorPositionStart };
+      }
+    } else {
+      return { isOn, mdText, cursorPositionStart };
+    }
+  };
+
   const newHandleButtonClick = (button) => {
     editorRef.current.focus();
     setButton((prevState) => ({ ...prevState, [button]: !isButtonOn[button] }));
@@ -119,7 +162,7 @@ const Buttons = ({
           break;
         }
 
-        results = bold(
+        results = emphasisButton(
           isButtonOn[button],
           mdText,
           cursorPositionStart,
@@ -157,7 +200,66 @@ const Buttons = ({
           break;
         }
 
-        results = bold(
+        results = emphasisButton(
+          isButtonOn[button],
+          mdText,
+          cursorPositionStart,
+          cursorPositionEnd,
+          cursorIntON,
+          cursorIntOFF,
+          output
+        );
+
+        setChanges(
+          results.mdText,
+          results.cursorPositionStart,
+          results.cursorPositionEnd
+        );
+        break;
+      case "heading":
+        cursorIntON = emphasis[2].cursorIntON;
+        cursorIntOFF = emphasis[2].cursorIntOFF;
+        output = emphasis[2].output;
+
+        results = heading(
+          isButtonOn[button],
+          mdText,
+          cursorPositionStart,
+          output
+        );
+        console.log(results);
+        setButton((prevState) => ({ ...prevState, [button]: results.isOn }));
+        setChanges(
+          results.mdText,
+          results.cursorPositionStart,
+          results.cursorPositionStart
+        );
+
+        break;
+
+      case "strikethrough":
+        cursorIntON = emphasis[3].cursorIntON;
+        cursorIntOFF = emphasis[3].cursorIntOFF;
+        output = emphasis[3].output;
+
+        cancelResults = cancelButton(
+          isButtonOn[button],
+          mdText,
+          cursorPositionStart,
+          cursorPositionEnd,
+          cursorIntON,
+          output
+        );
+        if (cancelResults.cancel) {
+          setChanges(
+            cancelResults.mdText,
+            cancelResults.cursorPositionStart,
+            cancelResults.cursorPositionEnd
+          );
+          break;
+        }
+
+        results = emphasisButton(
           isButtonOn[button],
           mdText,
           cursorPositionStart,
