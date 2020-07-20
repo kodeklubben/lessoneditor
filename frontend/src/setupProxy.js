8cb7318fa562ce049e0c2912ee9fc2b896c466e8
BBrekken <benjamab@stud.ntnu.no>
2020-07-20 21:14:58 +0200

    fixed npm start from frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
+  require("../../backend/routes/auth.local")(app);
   require("../../backend/routes/configure")(app);
   require("../../backend/routes/current-user.local")(app);
   require("../../backend/routes/lesson-data")(app);
   require("../../backend/routes/lesson-proxy")(app);
   require("../../backend/routes/lesson-uploads.local")(app);
-  require("../../backend/routes/lesson-submit")(app);
   require("../../backend/routes/oppgaver.local")(app);
 };
