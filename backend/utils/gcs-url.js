4b36c0823236e5daf4f9b7a7977ac3e4ecb29b5f
Michael Johansen <michael.johansen@nav.no>
2020-07-21 20:06:40 +0200

    Flytter konstanter til env vars

--- backend/utils/gcs-url.js
+++ backend/utils/gcs-url.js
@@ -1,6 +1,5 @@
 const { format } = require("util");
-const constants = require("../constants.json");
 module.exports = (filename, bucket) => {
-  const bucketName = bucket || constants.BUCKET;
-  return format(`${constants.GOOGLE_STORAGE_PREFIX}/${bucketName}/${filename}`);
+  const bucketName = bucket || process.env.BUCKET;
+  return format(`https://storage.googleapis.com/${bucketName}/${filename}`);
 };
