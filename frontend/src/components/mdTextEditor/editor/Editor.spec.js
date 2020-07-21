c0e3a9f412d1a8050587999ea231fbd06e2d5330
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-07-21 15:48:44 +0200

    fix test fail

--- frontend/src/components/mdTextEditor/editor/Editor.spec.js
+++ frontend/src/components/mdTextEditor/editor/Editor.spec.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
 import { MemoryRouter } from "react-router-dom";
-import Editor from "components/mdTextEditor/editor/tempEditor";
+import Editor from "components/mdTextEditor/editor/Editor";
 
 let wrapped;
 
