cffd1c66fb101035fe2c272b2b551aad593eedcb
Michael Johansen <michael.johansen@nav.no>
2020-07-21 20:21:10 +0200

    Flytter konstanter til env vars (#108)

---
+++ backend/utils/github-lesson-url.js
@@ -0,0 +1,10 @@
+module.exports = (pathParts) => {
+  const pathPartsClone = [...pathParts];
+  pathPartsClone.unshift(
+    "https://raw.githubusercontent.com",
+    process.env.GITHUB_LESSON_REPO_OWNER,
+    process.env.GITHUB_LESSON_REPO,
+    "master/src"
+  );
+  return pathPartsClone.join("/");
+};
--- backend/utils/thumb-url.js
+++ backend/utils/thumb-url.js
@@ -1,9 +1,6 @@
 const createJwtToken = require("../auth/create-jwt-token");
 module.exports = (username, secret, previewUrl) => {
-  const serviceUrl = process.env.THUMB_SERVICE_URL
-    ? process.env.THUMB_SERVICE_URL
-    : "http://localhost:3012";
-  const url = new URL(serviceUrl);
+  const url = new URL(process.env.THUMB_SERVICE_URL);
   const token = createJwtToken(username, secret);
   url.searchParams.append("url", previewUrl);
   url.searchParams.append("token", token);
