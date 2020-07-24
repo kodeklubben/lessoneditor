4660ee454a38a0eade6e3a7646dbe7b5fcc207c6
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-07-24 15:05:14 +0200

    change path in tests

--- frontend/src/components/editor/Editor.spec.js
+++ frontend/src/components/editor/Editor.spec.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { mount } from "enzyme";
 import { MemoryRouter } from "react-router-dom";
-import Editor from "components/editor/editor/Editor";
+import Editor from "components/editor/Editor";
 
 let wrapped;
 
