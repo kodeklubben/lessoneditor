516078f2b056c4a7f29a903f58e6f68eca007580
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-08-05 10:51:25 +0200

    Toggle button (#141)
    * fix course in URL
    
    * improve Scratch rendering, some new style
    
    * save work
    
    * add section save, and some fixes
    
    * add toggleButton in preview. Temporary solution because markdownIT allows HTML tags

--- frontend/src/utils/renderToggleButton.js
+++ frontend/src/utils/renderToggleButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ToggleButton from "components/ToggleButton";
+import ToggleButton from "components/editor/ToggleButton";
 
 export const renderToggleButtons = () => {
-  const nodes = [...document.getElementsByClassName("togglebutton")];
+  const nodes = [...document.getElementsByTagName("toggle")];
   for (let node of nodes) {
     const strongNode = node.getElementsByTagName("strong")[0];
     const buttonText = strongNode ? strongNode.textContent : "Hint";
