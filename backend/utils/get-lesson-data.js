35673b0dfdcac6370fc4cce9a336fdbe83a61029
BBrekken <benjamab@stud.ntnu.no>
2020-08-20 22:07:36 +0200

    added handling of metadata for submit

--- backend/utils/get-lesson-data.js
+++ backend/utils/get-lesson-data.js
@@ -6,29 +6,29 @@ const yaml = require("js-yaml");
 // Todo: Check for empty fileList
 module.exports = async (lessonId) => {
   const fileList = await listFiles(["drafts", lessonId]);
-  const lessonData = {};
+  const lessonData = { meta: { lessonId: lessonId } };
   const lessonFiles = [];
-  lessonData.meta = getMetaData(lessonId);
   for (let i in fileList) {
+    if (fileList[i].filename === "preview.png") {
+      continue;
+    }
     if (fileList[i].filename === "data.json") {
-      fileList.splice(i, 0);
+      const metadata = JSON.parse(
+        await loadFile(["drafts", lessonId, fileList[i].filename])
+      );
+      lessonData.meta.course = metadata.course;
+      lessonData.meta.title = metadata.lesson;
+      const data = {};
+      data.ext = "yml";
+      data.filename = "lesson";
+      data.content = yaml.safeDump(metadata.yml, { flowLevel: 2 });
+      lessonFiles.push(data);
       continue;
     }
     const data = {};
     data.ext = fileList[i].filename.split(".").pop();
-    if (data.ext === "json") {
-      data.ext = "yml";
-      let filename = fileList[i].filename.split(".");
-      filename.pop();
-      data.filename = `${filename}.${data.ext}`;
-      data.content = yaml.safeDump(
-        JSON.parse(await loadFile(["drafts", lessonId, fileList[i].filename])),
-        { flowLevel: 2 }
-      );
-    } else {
-      data.filename = fileList[i].filename;
-      data.content = await loadFile(["drafts", lessonId, data.filename]);
-    }
+    data.filename = fileList[i].filename;
+    data.content = await loadFile(["drafts", lessonId, data.filename]);
     lessonFiles.push(data);
   }
   lessonData.files = lessonFiles;
