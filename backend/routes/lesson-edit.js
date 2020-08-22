f32ef4926fef0e98a5c3ed1b93aeb18d677b8a61
BBrekken <benjamab@stud.ntnu.no>
2020-08-22 17:09:17 +0200

    lessons.json oppdateres ikek

--- backend/routes/lesson-edit.js
+++ backend/routes/lesson-edit.js
@@ -1,7 +1,9 @@
 const getContent = require("../githubAPI/getRepoContent");
 const saveFile = require("../utils/save-file");
+const updateUserLessons = require("../utils/update-user-lessons");
 const axios = require("axios");
 const downloadImage = require("../utils/download-image");
+const isLessonSaved = require("../utils/is-lesson-saved");
 const nanoid = require("nanoid").customAlphabet(
   "01234567890abcdefghijklmnopqrstuvwxyz",
   7
@@ -9,38 +11,51 @@ const nanoid = require("nanoid").customAlphabet(
 const imgRegex = /([a-zA-Z0-9\s_\\.\-:])+(.png|.jpeg|.jpg)$/i;
 
 module.exports = (app) => {
-  app.post("/api/lesson/edit/:course/:lesson/:file", async (req, res) => {
+  app.get("/api/lesson/edit/:course/:lesson/:file", async (req, res) => {
     const { course, lesson, file } = req.params;
-    const path = `src/${course}/${lesson}`;
-    const files = await getContent(req.user.token, path);
-    if (files) {
-      const data = Object.assign({}, req.body);
-      data.lessonId = nanoid();
-      data.created = new Date().toISOString();
-      data.updated = new Date().toISOString();
-      data.createdBy = req.user.username;
-      const buffer = Buffer.from(JSON.stringify(data));
-      await saveFile(["drafts", data.lessonId, "data.json"], buffer);
-      for (const i in files.data) {
-        if (files.data[i].name.match(imgRegex)) {
-          const imageBuffer = await downloadImage(files.data[i].download_url);
-          await saveFile(
-            ["drafts", data.lessonId, files.data[i].name],
-            imageBuffer
-          );
-        } else {
-          const fileBuffer = await axios.get(files.data[i].download_url);
-          await saveFile(
-            ["drafts", data.lessonId, files.data[i].name],
-            Buffer.from(fileBuffer.data)
-          );
-        }
-      }
+    const savedLesson = await isLessonSaved(req.user.username, course, lesson);
+    if (savedLesson) {
       res.status(200).send({
-        redirect: `localhost:3232/api/lesson/edit/${data.lessonId}/${file}`,
+        redirect: `/api/lesson/edit/${savedLesson}/${file}`,
       });
     } else {
-      res.send("Error");
+      const path = `src/${course}/${lesson}`;
+      const files = await getContent(req.user.token, path);
+      if (files) {
+        const data = Object.assign({}, req.body);
+        data.lessonId = nanoid();
+        data.created = new Date().toISOString();
+        data.updated = new Date().toISOString();
+        data.createdBy = req.user.username;
+        const dataBuffer = Buffer.from(JSON.stringify(data));
+        await saveFile(["drafts", data.lessonId, "data.json"], dataBuffer);
+        await updateUserLessons(
+          data.lessonId,
+          course,
+          lesson,
+          req.user.username
+        );
+        for (const i in files.data) {
+          if (files.data[i].name.match(imgRegex)) {
+            const imageBuffer = await downloadImage(files.data[i].download_url);
+            await saveFile(
+              ["drafts", data.lessonId, files.data[i].name],
+              imageBuffer
+            );
+          } else {
+            const fileBuffer = await axios.get(files.data[i].download_url);
+            await saveFile(
+              ["drafts", data.lessonId, files.data[i].name],
+              Buffer.from(fileBuffer.data)
+            );
+          }
+        }
+        res.status(200).send({
+          redirect: `/api/lesson/edit/${data.lessonId}/${file}`,
+        });
+      } else {
+        res.send("Error");
+      }
     }
   });
 };
