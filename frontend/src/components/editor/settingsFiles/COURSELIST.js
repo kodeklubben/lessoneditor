6d6b466e280bdd3426992140688f73f6a18ad90b
Erik Sagatun <eriksaga@stud.ntnu.no>
2020-11-04 21:13:19 +0100

    restrict project title, fix image upload (#232)
    * restrict project title, fix image upload
    
    * slugify lessonTitle

--- frontend/src/components/editor/settingsFiles/COURSELIST.js
+++ frontend/src/components/editor/settingsFiles/COURSELIST.js
@@ -1,7 +1,7 @@
-import Axios from "axios";
-import yaml from "js-yaml";
+// import Axios from "axios";
+// import yaml from "js-yaml";
 
-const COURSESLISTold = [
+const COURSESLIST = [
   { courseTitle: "App Inventor", slug: "appinventor" },
   { courseTitle: "Arduino", slug: "arduino" },
   { courseTitle: "Code Studio", slug: "codestudio" },
@@ -16,51 +16,51 @@ const COURSESLISTold = [
   { courseTitle: "Uten Datamaskin", slug: "uten_datamaskin" },
 ];
 
-const GITHUB_URL =
-  "https://api.github.com/repos/kodeklubben/oppgaver/contents/src/";
+// const GITHUB_URL =
+//   "https://api.github.com/repos/kodeklubben/oppgaver/contents/src/";
 
-let COURSESLIST = [];
+// let COURSESLIST = [];
 
-async function fecthData() {
-  const getCourseList = () =>
-    Axios.get(GITHUB_URL)
-      .then((response) => response.data)
-      .catch((error) => {
-        console.log(error);
-        COURSESLIST = COURSESLISTold;
-      });
+// async function fecthData() {
+//   const getCourseList = () =>
+//     Axios.get(GITHUB_URL)
+//       .then((response) => response.data)
+//       .catch((error) => {
+//         console.log(error);
+//         COURSESLIST = COURSESLISTold;
+//       });
 
-  const test1 = await getCourseList();
+//   const test1 = await getCourseList();
 
-  const getCourseTitle = (i) =>
-    Axios.get(GITHUB_URL + i.name + "/index.md")
-      .then((response) => {
-        let buff = new Buffer.from(response.data.content, "base64");
-        let text = yaml.safeLoad(buff.toString("utf8").split("---")[1]);
-        return text.title;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+//   const getCourseTitle = (i) =>
+//     Axios.get(GITHUB_URL + i.name + "/index.md")
+//       .then((response) => {
+//         let buff = new Buffer.from(response.data.content, "base64");
+//         let text = yaml.safeLoad(buff.toString("utf8").split("---")[1]);
+//         return text.title;
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
 
-  if (typeof test1 !== "undefined") {
-    for (let i of test1) {
-      if (i.type === "dir") {
-        if (i.name === "sponsorer") continue;
-        const title = await getCourseTitle(i);
-        COURSESLIST = [
-          ...COURSESLIST,
-          {
-            courseTitle: title,
-            slug: i.name,
-          },
-        ];
-      }
-    }
-  } else {
-    COURSESLIST = COURSESLISTold;
-  }
-}
-fecthData();
+//   if (typeof test1 !== "undefined") {
+//     for (let i of test1) {
+//       if (i.type === "dir") {
+//         if (i.name === "sponsorer") continue;
+//         const title = await getCourseTitle(i);
+//         COURSESLIST = [
+//           ...COURSESLIST,
+//           {
+//             courseTitle: title,
+//             slug: i.name,
+//           },
+//         ];
+//       }
+//     }
+//   } else {
+//     COURSESLIST = COURSESLISTold;
+//   }
+// }
+// fecthData();
 
 export { COURSESLIST };
