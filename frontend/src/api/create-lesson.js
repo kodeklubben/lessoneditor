6dc78e635a760ed27d266f2c71aef906d9973afc
Michael Johansen <michael.johansen@gmail.com>
2021-05-13 20:41:14 +0200

    manually updateing dependencies (#299)

--- frontend/src/api/create-lesson.js
+++ frontend/src/api/create-lesson.js
@@ -1,7 +1,9 @@
-import paths from "../paths.json";
-import axios from "axios";
+import paths from '../paths.json';
+import axios from 'axios';
 
-export default async (data) => {
+const createLesson = async (data) => {
   const result = await axios.post(paths.LESSON, data);
   return result.data.lessonId;
 };
+
+export default createLesson;
