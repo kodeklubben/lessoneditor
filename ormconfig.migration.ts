8366a5c07f229af33c74f5de157d479b1fa0ffda
Erik Brendbekken Rudlang <erik.rudlang@bouvet.no>
2021-11-12 13:18:50 +0100

    add env variables and fix workflow

--- ormconfig.migration.ts
+++ ormconfig.migration.ts
@@ -2,9 +2,9 @@ module.exports = {
   "type": "postgres",
   "host": "localhost",
   "port": 5432,
-  "username": "github-actions@lessoneditor.iam",
-  "database": "lessoneditor",
-  "password": "ya29.c.KqYBGAgyR4D20qIMbqYVRipWGMUBSILWbfs2QpONHtguKDjLJJQqv_eSNWwcmSOr4rNnHHzkZLDuHJpdCubPPn0sJ6XxACJa20dqKr7Qjwb0ihMtS3HjQzFLBXhzU_VzhkhRCiv2GsHAW_WLwRYthKSLmg638bH2NWbThgJ0yCrbKB9K7yZe0RfdiPReNeWGEzqsgJw3UoFLvorPWiwN0eN9T_gjQBGEvA.....................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................",
+  "username": "orm-user",
+  "database": "lesson-editor",
+  "password": "testing",
   "synchronize": false,
   "logging": false,
   "entities": [
