ead2cc97567160822a3331a6aa142e07d5a963b9
sagatun <eriksaga@stud.ntnu.no>
2021-11-29 14:54:45 +0100

    add Explorer component

---
+++ apps/frontend/src/app/components/editor/buttonpanel/Explorer.tsx
@@ -0,0 +1,66 @@
+import { FC, useState, RefObject } from "react";
+import { RenderButtons } from "./buttoncontroller/views/RenderButtons";
+import { useHotkeys } from "react-hotkeys-hook";
+import {
+  fileExplorer as config,
+  KEY_COMBINATIONS as KEY,
+} from "./buttoncontroller/settings/buttonConfig";
+import ListFiles from "../../shared/ListFiles";
+import { useLessonContext } from "../../../contexts/LessonContext";
+
+import { Modal, Container } from "semantic-ui-react";
+
+const Explorer: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { state } = useLessonContext();
+
+  const filterItems = ["preview.png", "lesson.yml", "data.json", "image.png"];
+
+  useHotkeys(
+    KEY.hyperlink.hyperlink,
+    (event) => {
+      event.preventDefault();
+      handleButtonClick();
+    },
+    { enableOnTags: ["TEXTAREA"], keydown: true }
+  );
+
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen);
+    return;
+  };
+
+  return (
+    <>
+      <RenderButtons
+        isON={false}
+        icon={config.explorer.icon}
+        title={config.explorer.title}
+        handleButtonClick={handleButtonClick}
+        buttonSlug={config.explorer.slug}
+        shortcutKey={config.explorer.shortcut}
+      />
+
+      <Modal
+        onClose={() => setIsOpen(false)}
+        onOpen={() => setIsOpen(true)}
+        closeIcon
+        open={isOpen}
+        size="small"
+        className="hyperlink_modal"
+      >
+        <Container style={{ padding: "2em" }}>
+          <ListFiles
+            list={
+              state.files?.length > 0
+                ? state.files?.filter((fileName: string) => !filterItems.includes(fileName))
+                : ["No files found"]
+            }
+          />
+        </Container>
+      </Modal>
+    </>
+  );
+};
+
+export default Explorer;
