64d2219c4e74d2641b00323cd683c7dcdc142171
Erik Brendbekken Rudlang <erik.rudlang@bouvet.no>
2021-12-03 16:49:28 +0100

    remove backend project

--- apps/backend/src/app/auth/whitelist.ts
+++
@@ -1,1 +0,0 @@
-export default ["/callback", "/login-failed"];
--- apps/backend/src/app/utils/gcs-url.ts
+++
@@ -1,8 +0,0 @@
-const {format} = require("util");
-
-const gcsUrl = (filename, bucket?) => {
-    const bucketName = bucket || process.env.BUCKET;
-    return format(`https://storage.googleapis.com/${bucketName}/${filename}`);
-};
-
-export default gcsUrl;
