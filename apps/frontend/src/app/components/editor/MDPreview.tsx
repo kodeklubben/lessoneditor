6a41b1cfd7dc8758cd7b77b6173f92e9d93ae938
Erik Sagatun <eriksagatun@gmail.com>
2022-02-25 11:28:35 +0100

    New landingpage (#471)
    * newLandingPAge WIP
    
    * newLandingPAge WIP
    
    * solve deploy problem
    
    * solve deploy problem
    
    * solve deploy problem
    
    * prevent template-image to get submitted
    
    * fix headerdata in new lessontexts
    
    * add favicon + style
    
    * fix imagePath and style
    
    * fix imagePath and style
    
    * save changes
    
    * fix scratchblock performance

--- apps/frontend/src/app/components/editor/MDPreview.tsx
+++ apps/frontend/src/app/components/editor/MDPreview.tsx
@@ -5,6 +5,7 @@ import { renderScratchBlocks } from "../../utils/renderScratchblocks";
 import { mdParser } from "../../utils/mdParser";
 import { useLessonContext } from "../../contexts/LessonContext";
 import ContentPlaceholder from "./ContentPlaceholder";
+import md5 from "crypto-js/md5";
 
 interface MDPreviewProps {
   mdText: string;
@@ -12,34 +13,81 @@ interface MDPreviewProps {
   language: string;
 }
 
+let storeSVG = {};
+
 const MDPreview: FC<MDPreviewProps> = ({ mdText, course, language }) => {
   const { images } = useLessonContext();
 
-  const [mdTextUrlReplaced, setMdTextUrlReplaced] = useState<string>("");
-  const parseMD = mdTextUrlReplaced && mdParser(mdTextUrlReplaced);
+  const [svgTest, setSvgTest] = useState<any>("");
+
+  const [parsedMDwithUpdatedImageURLS, setParsedMDwithUpdatedImageURLS] = useState<string>("");
 
   useEffect(() => {
     if (course === "microbit") {
       renderMicrobit(language);
     }
-  }, [parseMD]);
+  }, [mdText]);
+
+  useEffect(() => {
+    if (course !== "scratch") {
+      return;
+    }
+    const timeoutHandler = setTimeout(async () => {
+      function replaceScratchBlocksWithSVG() {
+        const replace = {
+          start: '<pre><code class="blocks">',
+          end: "</code></pre>",
+        };
+        let returnContent = parsedMDwithUpdatedImageURLS;
+        const re = new RegExp(replace.start + "[\\s\\S]*?" + replace.end, "g");
+        let blocks = parsedMDwithUpdatedImageURLS.match(re);
+        if (blocks) {
+          blocks.forEach((block: any) => {
+            let code = block.substring(replace.start.length, block.length - replace.end.length);
+            const checksum = md5(code).toString();
+            if (checksum in storeSVG) {
+              // @ts-ignore
+              returnContent = returnContent.replace(block, storeSVG[checksum]);
+              setSvgTest(returnContent);
+            } else {
+              returnContent = renderScratchBlocks(parsedMDwithUpdatedImageURLS);
+              const test = returnContent.slice(
+                returnContent.indexOf("<svg"),
+                returnContent.indexOf("</svg>") + "</svg>".length
+              );
+              // @ts-ignore
+              storeSVG[checksum] = test;
+              setSvgTest(returnContent);
+            }
+          });
+        } else {
+          setSvgTest(returnContent);
+        }
+      }
+
+      replaceScratchBlocksWithSVG();
+    }, 250);
+    return () => {
+      clearTimeout(timeoutHandler);
+    };
+  }, [parsedMDwithUpdatedImageURLS]);
 
   useEffect(() => {
     function replaceUrlWithBlobUrl(markdownContent: any) {
-      if (!/(!\[.*?\]\(")(.+?)("\))/.test(markdownContent)) {
+      if (!/(!\[.*?\])(\(\.\/.*?\))/.test(markdownContent)) {
         return markdownContent;
       }
       const filenames: string[] = [];
       markdownContent.replace(
-        /(!\[.*?\]\(")(.+?)("\))/gs,
+        /(!\[.*?\])(\(\.\/.*?\))/gs,
         function (whole: string, prefix: string, imagePathRaw: string, postfix: string) {
-          filenames.push(imagePathRaw);
+          filenames.push(imagePathRaw.slice(3, -1));
         }
       );
       return markdownContent.replace(
         /(!\[.*?\]\()(.+?)(\))/gs,
         function (whole: string, prefix: string, imagePathRaw: string, postfix: string) {
-          imagePathRaw = imagePathRaw.slice(1, -1);
+          imagePathRaw = imagePathRaw.slice(2);
           const filename: string = filenames.shift() ?? "";
           const imageBlobUrl = images[filename];
           return prefix + imageBlobUrl + postfix;
@@ -49,26 +97,27 @@ const MDPreview: FC<MDPreviewProps> = ({ mdText, course, language }) => {
 
     async function replaceUrls() {
       const newMdText = replaceUrlWithBlobUrl(mdText);
-      setMdTextUrlReplaced(newMdText);
+      setParsedMDwithUpdatedImageURLS(mdParser(newMdText));
     }
     replaceUrls();
-
-    if (course === "microbit") {
-      renderMicrobit(language);
-    }
   }, [mdText]);
 
-  if (course === "scratch" && parseMD) {
-    const lessonContent = renderScratchBlocks(parseMD);
+  if (course === "scratch" && parsedMDwithUpdatedImageURLS) {
+    const lessonContent = renderScratchBlocks(svgTest);
     return <div className="preview-area" dangerouslySetInnerHTML={{ __html: lessonContent }} />;
-  } else if (!parseMD && mdText !== "") {
+  } else if (!parsedMDwithUpdatedImageURLS && mdText !== "") {
     return (
       <div className="preview-area">
         <ContentPlaceholder />
       </div>
     );
   } else {
-    return <div className="preview-area" dangerouslySetInnerHTML={{ __html: parseMD }} />;
+    return (
+      <div
+        className="preview-area"
+        dangerouslySetInnerHTML={{ __html: parsedMDwithUpdatedImageURLS }}
+      />
+    );
   }
 };
 
